4e705b153361e614780ff312fb13cec02ab759c6
foomoon <daniel.j.claxton@gmail.com>
2022-05-20 14:26:35 -0500

    better parsing

--- ui/src/data-logger.js
+++ ui/src/data-logger.js
@@ -1,30 +1,51 @@
 
 function parseData(msg) {
+
+  // expect whole lines but could be multiple lines
+  let lines = msg.split("\n");
+  // initialize output array
+  let output = [];
+  // expect 28 fields per line
   let correctNumberOfFields = 28;
-  if (msg.length > 25) {
-    let fields = msg.split(",");
-    if (fields.length == correctNumberOfFields) {
-      return {
-        utc: fields[0],
-        met: fields[1] / 1000,    // sec
-        lat: fields[2] / 10000000, // deg
-        lng: fields[3] / 10000000, // deg
-        alt: fields[4] / 1000,    // m
-        speed: fields[5] / 1000,  // m/s
-        heading: fields[6] / 100000, // deg
-        siv: fields[7]*1 + 0,
-        pressureRaw: getRawPressures(fields),
-        temperatureRaw: getRawTemperatures(fields),
-        heightRaw: getRawHeights(fields)
+
+  // loop through each line found
+  for (let i = 0; i < lines.length-1; i++) {
+
+    msg = lines[i].trim();
+    let lineNum = i + 1;
+    let totalLines = lines.length -1;
+    console.log("LINE " + lineNum + "/" + totalLines + " " + msg)
+
+    // check if line is legit long enough to be interesting
+    if (msg.length > 25) {
+      // split each line into fields
+      let fields = msg.split(",");
+      // if correct number of fields
+      if (fields.length == correctNumberOfFields) {
+        output.push ({
+          utc: fields[0],
+          met: fields[1] / 1000,    // sec
+          lat: fields[2] / 10000000, // deg
+          lng: fields[3] / 10000000, // deg
+          alt: fields[4] / 1000,    // m
+          speed: fields[5] / 1000,  // m/s
+          heading: fields[6] / 100000, // deg
+          siv: fields[7]*1 + 0,
+          pressureRaw: getRawPressures(fields),
+          temperatureRaw: getRawTemperatures(fields),
+          heightRaw: getRawHeights(fields)
+        })
+      } else {
+        console.warn("Incorrect number of fields! Found " + fields.length + ", expected: " + correctNumberOfFields);
       }
     } else {
-      console.warn("Incorrect number of fields! Found " + fields.length + ", expected: " + correctNumberOfFields);
-      return {}
+      console.warn("Message not formatted correctly or too short")
+      //console.log(msg)
     }
-  } else {
-    console.warn("Message not formatted correctly or too short")
-    return {}
-  }
+
+  } // end for
+
+  return output
 }
 
 function getRawData(fields,start,stop,interval) {
